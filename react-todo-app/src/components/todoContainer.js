93ce0fa4d4db48eb6d793df8d470119c4e940bd3
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2022-03-30 12:24:31 +0530

    todo list

--- react-todo-app/src/components/todoContainer.js
+++ react-todo-app/src/components/todoContainer.js
@@ -1,11 +1,39 @@
 import React from "react"
+import Header from "./header"
+import TodosList from "./TodosList"
+
+
+const state = {
+  todos: [
+    {
+      id: 1,
+      title: "Setup development environment",
+      completed: true
+    },
+    {
+      id: 2,
+      title: "Develop website and add content",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Deploy to live server",
+      completed: false
+    },
+    {
+      id: 4,
+      title: "UAT to client",
+      completed: false
+    }
+  ]
+ };
 class TodoContainer extends React.Component {
+  
   render() {
     return (
       <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </div>
+      <Header />      <TodosList todos={state.todos} />
+    </div>
     )
   }
 }
