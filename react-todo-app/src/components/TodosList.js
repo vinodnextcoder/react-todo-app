ec6957e41405c25620ab5e69afc6d1f011893650
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2022-03-30 12:32:28 +0530

    checked box added

--- react-todo-app/src/components/TodosList.js
+++ react-todo-app/src/components/TodosList.js
@@ -1,11 +1,15 @@
 import React from "react"
 
+const handleChange = () => {
+  console.log("clicked");
+};
 class TodosList extends React.Component {
   render() {
     return (
       <ul>
         {this.props.todos.map(todo => (
-          <li>{todo.title}</li>
+          <li><input type="checkbox" /> {todo.title}</li>
+          
         ))}
       </ul>
     )
