9593cefe52a0e736e82b48a1021ac2287a76e242
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2022-03-31 12:57:31 +0530

    multiple input fild

--- react-form-handling/src/App.js
+++ react-form-handling/src/App.js
@@ -2,18 +2,24 @@ import React, { useState } from "react"
 import "./App.css"
 
 function App() {
-  const [fname, setFname] = useState("")
+  const [state, setState] = useState({    fname: "",    lname: "",  })
+  const handleChange = e => {
+    setState({      ...state,      [e.target.name]: e.target.value,    })  }
 
-  const handleChange = e => {    setFname(e.target.value)  }
   return (
     <div>
-      <h1>React Form Handling</h1>
+      <h1 style={{color: "blue"}}>React Form Handling</h1>
       <form>
         <label>
           First Name:{" "}
-          <input type="text" value={fname} onChange={handleChange} />        </label>
-      </form>
-      <h5>First name: {fname}</h5>
+          <input
+            type="text"
+            name="fname"            value={state.fname}            onChange={handleChange}
+          />
+        </label>{" "}<br></br>
+        <label>          Last Name:{" "}          <input            type="text"            name="lname"            value={state.lname}            onChange={handleChange}          />        </label>      </form>
+      <h5>
+        Name: {state.fname} {state.lname}      </h5>
     </div>
   )
 }
