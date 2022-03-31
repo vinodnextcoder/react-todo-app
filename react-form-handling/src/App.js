dee7b87e8737f2107abe812fadb73a71e2547826
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2022-03-31 12:52:01 +0530

    onchange event

--- react-form-handling/src/App.js
+++ react-form-handling/src/App.js
@@ -1,15 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [fname, setFname] = useState("")
+
+  const handleChange = e => {    setFname(e.target.value)  }
   return (
     <div>
       <h1>React Form Handling</h1>
       <form>
         <label>
-          First Name: <input type="text" />
-        </label>
+          First Name:{" "}
+          <input type="text" value={fname} onChange={handleChange} />        </label>
       </form>
+      <h5>First name: {fname}</h5>
     </div>
   )
 }
