95aadddb46e4a289fb26b0cdce126e58b0211d5d
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2022-03-31 13:14:38 +0530

    form added with multple inputs

--- react-form-handling/src/App.js
+++ react-form-handling/src/App.js
@@ -1,27 +1,139 @@
-import React, { useState } from "react"
-import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [state, setState] = useState({    fname: "",    lname: "",  })
-  const handleChange = e => {
-    setState({      ...state,      [e.target.name]: e.target.value,    })  }
+  const [state, setState] = useState({
+    fname: "",
+    lname: "",
+    message: "",
+    carBrand: "",
+    isChecked: false,
+    gender: "",
+    price: 0,
+  });
+
+  const handleChange = (e) => {
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: value,
+    });
+  };
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans-Serif"
+  };
 
   return (
-    <div>
-      <h1 style={{color: "blue"}}>React Form Handling</h1>
+    <div  className="wrapper">
+      <h1 style={myStyle}>React Form Handling</h1>
       <form>
         <label>
           First Name:{" "}
           <input
             type="text"
-            name="fname"            value={state.fname}            onChange={handleChange}
+            name="fname"
+            value={state.fname}
+            onChange={handleChange}
           />
-        </label>{" "}<br></br>
-        <label>          Last Name:{" "}          <input            type="text"            name="lname"            value={state.lname}            onChange={handleChange}          />        </label>      </form>
+        </label>{" "}
+        <br></br>
+        <label>
+          Last Name:{" "}
+          <input
+            type="text"
+            name="lname"
+            value={state.lname}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        
+        <label>
+          Your Message:{" "}
+          <textarea
+            name="message"
+            value={state.message}
+            onChange={handleChange}
+          />
+        </label>
+      
+        <br />
+        <br />
+        <label>
+          Pick your favorite car brand:{" "}
+          <select
+            name="carBrand"
+            value={state.carBrand}
+            onChange={handleChange}
+          >
+            <option value="mercedes">Mercedes</option>
+            <option value="bmw">BMW</option>
+            <option value="maserati">Maserati</option>
+            <option value="infinity">Infinity</option>
+            <option value="audi">Audi</option>
+          </select>
+        </label>
+        <br />
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="isChecked"
+            checked={state.isChecked}
+            onChange={handleChange}
+          />{" "}
+          Is Checked?
+        </label>
+        <br />
+        <br />
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={state.gender === "male"}
+            onChange={handleChange}
+          />{" "}
+          Male
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={state.gender === "female"}
+            onChange={handleChange}
+          />{" "}
+          Female
+        </label>
+        <br />
+        <br />
+        <label>
+          Price (between 0 and 50):
+          <input
+            type="range"
+            name="price"
+            min="0"
+            max="50"
+            value={state.price}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
       <h5>
-        Name: {state.fname} {state.lname}      </h5>
+        Name: {state.fname} {state.lname}
+      </h5>
+      <h5>My favorite car brand: {state.carBrand}</h5>
+      <p>Message: {state.message}</p>
+      <h5>Is it checked? : {state.isChecked ? "Yes" : "No"}</h5>
+      <h5>Gender Selected : {state.gender}</h5>
+      <h5>Price : ${state.price}</h5>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
